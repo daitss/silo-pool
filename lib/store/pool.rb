6c28c38c5f84372f8034b9f5e6badbd449e735b2
Randy Fischer <fischer@sacred.net>
2011-04-27 13:47:53 -0400

    extend fixity data to include the put time

--- lib/store/pool.rb
+++ lib/store/pool.rb
@@ -5,6 +5,13 @@ require 'store/db'
 
 module Store
 
+  class DateTime
+    def to_utc
+      new_offset(0).to_s.sub(/\+00:00$/, 'Z')
+    end
+  end
+
+
 # Class PoolFixity
 #
 # TODO: make silo-level fixities use this same strategem, include here.
@@ -16,7 +23,7 @@ module Store
   class PoolFixity
 
     Struct.new('FixityHeader', :hostname, :count, :earliest, :latest)
-    Struct.new('FixityRecord', :name, :location, :status, :md5, :sha1, :time, :size)
+    Struct.new('FixityRecord', :name, :location, :status, :md5, :sha1, :fixity_time, :put_time, :size)
 
     include Enumerable
 
@@ -69,6 +76,7 @@ module Store
                                          pkg.latest_md5,
                                          pkg.latest_sha1,
                                          pkg.latest_timestamp,
+                                         pkg.initial_timestamp,
                                          pkg.size
                                          )
         end
@@ -106,8 +114,12 @@ module Store
                          'sha1="'   + fix.sha1                            + '" '  +
                           'md5="'   + fix.md5                             + '" '  +
                          'size="'   + fix.size.to_s                       + '" '  +
-                         'time="'   + fix.time.to_s                       + '" '  +
+                  'fixity_time="'   + fix.fixity_time.to_utc              + '" '  +
+                     'put_time="'   + fix.put_time.to_utc                 + '" '  +
                        'status="'   + fix.status.to_s                     + '"/>' + "\n"
+
+
+
       end
 
       yield "</fixities>\n"
@@ -128,9 +140,9 @@ module Store
     end
 
     def each
-      yield '"name","location","sha1","md5","size","time","status"' + "\n"
+      yield '"name","location","sha1","md5","size","fixity_time","put_time","status"' + "\n"
       @pool_fixity.each do |r|
-        yield [r.name, r.location, r.sha1, r.md5, r.size.to_s, r.time.to_s, r.status.to_s].map{ |e| StoreUtils.csv_escape(e) }.join(',') + "\n"
+        yield [r.name, r.location, r.sha1, r.md5, r.size.to_s, r.fixity_time.to_utc, r.put_time.to_utc, r.status.to_s].map{ |e| StoreUtils.csv_escape(e) }.join(',') + "\n"
       end
     end
 
