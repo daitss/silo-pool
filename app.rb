a9e533af2ba81c939f8a0b467f281e12b240ef32
Randy Fischer <fischer@sacred.net>
2011-09-19 17:24:53 -0400

    minor reformatting & docs

--- app.rb
+++ app.rb
@@ -27,54 +27,42 @@ end
 configure do
   $KCODE = 'UTF8'
 
-  # boiler plate settings
+  set :logging,     false        # Stop CommonLogger from logging to STDERR
+  set :dump_errors, false        # Don't add backtraces automatically (we'll decide)
+  set :environment, :production  # Get some exceptional defaults.
+  set :raise_errors, false       # Let our app handle the exceptions.
 
-  disable :logging        # Stop CommonLogger from logging to STDERR, please.
-  disable :dump_errors    # Set to true in 'classic' style apps (of which this is one) regardless of :environment; it
-                          # adds a backtrace to STDERR on all raised errors (even those we properly handle). Not so good.
+  config = get_config()
 
-  set :environment,  :production             # Get some exceptional defaults.
-  set :raise_errors,  false                  # Handle our own errors
+  ENV['TMPDIR'] = config.temp_directory
 
-  # our app-specific settings:
-
-  config = get_config
-
-  ENV['TMPDIR'] = config.temp_directory if config.temp_directory
-
-  set :tivoli_server,              config.tivoli_server
-  set :silo_temp_directory,        config.silo_temp_directory   || '/var/tmp'
-  set :fixity_stale_days,          config.fixity_stale_days     || 45
-  set :fixity_expired_days,        config.fixity_expired_days   || 60
+  set :tivoli_server,         config.tivoli_server
+  set :silo_temp_directory,   config.silo_temp_directory   || '/var/tmp'
+  set :fixity_stale_days,     config.fixity_stale_days     || 45
+  set :fixity_expired_days,   config.fixity_expired_days   || 60
 
   Logger.setup 'SiloPool', ENV['VIRTUAL_HOSTNAME']
-
-  if not (config.log_filename or config.log_syslog_facility)
-    Logger.stderr
-  end
 
   Logger.facility = config.log_syslog_facility  if config.log_syslog_facility
   Logger.filename = config.log_filename         if config.log_filename
 
-  use Rack::CommonLogger, Logger.new(:info, 'Rack:')  # Bend CommonLogger to our will...
+  Logger.stderr unless (config.log_filename or config.log_syslog_facility)
+
+  use Rack::CommonLogger, Logger.new(:info, 'Rack:')  # Bend CommonLogger to our logging system
 
   Logger.info "Starting #{Store.version.name}; Tivoli server is #{settings.tivoli_server || 'not defined.' }."
   Logger.info "Using temp directory #{config.temp_directory}" if config.temp_directory
   Logger.info "Using database #{StoreUtils.safen_connection_string(config.silo_db)}"
-
-  # TODO: log rest of config options?
 
   DataMapper::Logger.new(Logger.new(:info, 'DataMapper:'), :debug) if config.log_database_queries
 
   Store::DB.setup config.silo_db
 end
 
-
 before do
   @started = Time.now
   raise Http401, 'You must provide a basic authentication username and password' if needs_authentication?
 end
-
 
 load 'lib/app/helpers.rb'
 load 'lib/app/errors.rb'
