20c778c3be8f540e77518ca470663a50f395249f
Randy Fischer <fischer@sacred.net>
2011-11-25 13:20:59 -0500

    added even more verbosory to DELETE failure error message

--- lib/app/deletes.rb
+++ lib/app/deletes.rb
@@ -2,7 +2,7 @@
 
 delete '/:partition/data/:name' do |partition, name|
   silo = get_silo(partition, name)
-  silo.delete_ok? or raise Http405
+  silo.delete_ok? or raise(Http405, "DELETEs are not currently allowed on silo #{partition} - you must enable them to delete #{name}")
   silo.delete(name)
   status 204
 end
