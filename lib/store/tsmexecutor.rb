0d1bf3754ac611ba1f64ef9db96ac02de1f7f840
Ira Terman <ira.terman1@gmail.com>
2012-06-05 14:00:56 -0400

    tapefixity use alternate silo_pool user name

--- lib/store/tsmexecutor.rb
+++ lib/store/tsmexecutor.rb
@@ -178,10 +178,15 @@ module Store
 
     ###  * -latest just seems to make sense -- oops - that returns inactive as well....
 
-    def restore path, destination, timeout = nil
+    def restore path, destination, tivoli_owner,timeout = nil
       ### run_dsmc(timeout, dsmc, "restore", "-latest", "-replace=no", "-filesonly", "-servername=#{server}", "-subdir=yes", path, destination)
+      path = '"' << path << "*"  << '"'
+      puts "tsmexecutor.rb in restore before run_dsmc  servername=#{server} path=#{path} destination=#{destination} tivoli_owner=#{tivoli_owner}"
 
-      run_dsmc(timeout, dsmc, "restore", "-replace=no", "-filesonly", "-servername=#{server}", "-subdir=yes", path, destination)
+      #run_dsmc(timeout, dsmc, "restore", "-replace=no", "-filesonly", "-servername=#{server}", "-subdir=yes",  "-fromowner=fcldem" , "-optfile=/opt/tivoli/tsm/client/ba/bin/iraserv.opt", path, destination)
+      #-worksrun_dsmc(timeout, dsmc, "restore", "-replace=no", "-filesonly", "-subdir=yes",  "-fromowner=fcldem" , "-optfile=/opt/tivoli/tsm/client/ba/bin/iraserv.opt", path, destination)
+      run_dsmc(timeout, dsmc, "restore", "-replace=no", "-filesonly", "-subdir=yes",  "-fromowner=#{tivoli_owner}" , "-servername=#{server}", path, destination)
+      puts "tsmexecutor.rb in restore after run_dsmc  servername=#{server} path=#{path} destination=#{destination} $!=#{$!}"
     end
     
     def output &blk
